0b9bdadcf47d4a19d9296b3a794ee7f9c40fa28a
Holger Bruch <hb@mfdz.de>
2025-08-24 08:51:09 +0200

    VVS: fix: enable impact_period start checking

--- gtfs_realtime_translators/translators/de_vvs.py
+++ gtfs_realtime_translators/translators/de_vvs.py
@@ -244,7 +244,7 @@ class DeVVSAlertGtfsRealtimeTranslator:
             impact_period_start = self.extract_impact_period_start(description)
         
         for period in entity.alert.active_period:
-            if period.HasField('start') and period.start <= latest_start_seconds:# and (impact_period_start is None or impact_period_start.timestamp() <= latest_start_seconds):
+            if period.HasField('start') and period.start <= latest_start_seconds and (impact_period_start is None or impact_period_start.timestamp() <= latest_start_seconds):
                 if not period.HasField('end') or period.end > now_timestamp:
                     return True
         return False
